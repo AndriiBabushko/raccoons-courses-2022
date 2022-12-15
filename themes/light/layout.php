0be4168b8803c8ce67a850d8fdfe3f4519175855
Andrii Babushko <andriibabushko@gmail.com>
2022-12-15 16:05:40 +0200

    A little modify themes layout files

--- themes/light/layout.php
+++ themes/light/layout.php
@@ -14,7 +14,7 @@
     <!--    ====================================================================================================================-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <!--    ====================================================================================================================-->
-    <link rel="stylesheet" href="/static/styles/css/light_theme.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="/static/styles/light/css/light_theme.css?v=<?php echo time(); ?>">
     <!--    ====================================================================================================================-->
 </head>
 <body>
@@ -22,7 +22,17 @@
     <nav class="navbar navbar-expand-lg navbar-light navbar-bg">
         <div class="container">
             <img src="/static/img/raccoon-logo-1.png" alt="logo" width="50" height="50" class="img-fluid">
-            <a href="/" class="navbar-brand pl-2 text-wrap">Raccoon's Programming Courses</a>
+            <?php
+            if (empty($language)) {
+                echo "
+                <a href='/site/eng/index' class='navbar-brand pl-2 text-wrap'>Raccoon's Programming Courses</a>
+                ";
+            } else {
+                echo "
+                <a href='/site/$language/index' class='navbar-brand pl-2 text-wrap'>Raccoon's Programming Courses</a>
+                ";
+            }
+            ?>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                     aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -61,99 +71,57 @@
                         </li>
                         ";
                     } else {
-                        echo "
-                        <li class='nav-item'> 
-                             <a class='nav-link' href='/user/form' data-toggle='tooltip' data-placement='top' title='Login'>
-                                <span><i class='bi bi-box-arrow-in-left fs-4'></i></span>
-                             </a>
-                        </li>
-                        ";
+                        if (empty($language)) {
+                            echo "
+                            <li class='nav-item'>
+                                <a class='nav-link' href='/user/eng/login' data-toggle='tooltip' data-placement='top' title='Login'>
+                                    <span><i class='bi bi-box-arrow-in-left fs-4'></i></span>
+                                </a>
+                            </li>
+                            ";
+
+                            echo "
+                            <li class='nav-item'>
+                                <a class='nav-link' href='/user/eng/register' data-toggle='tooltip' data-placement='top' title='Login'>
+                                    <span><i class='bi bi-person-plus fs-4'></i></span>
+                                </a>
+                            </li>
+                            ";
+                        } else {
+                            echo "
+                            <li class='nav-item'>
+                                <a class='nav-link' href='/user/$language/login' data-toggle='tooltip' data-placement='top' title='Login'>
+                                    <span><i class='bi bi-box-arrow-in-left fs-4'></i></span>
+                                </a>
+                            </li>
+                            ";
+
+                            echo "
+                            <li class='nav-item'>
+                                <a class='nav-link' href='/user/$language/register' data-toggle='tooltip' data-placement='top' title='Register'>
+                                    <span><i class='bi bi-person-plus fs-4'></i></span>
+                                </a>
+                            </li>
+                            ";
+                        }
                     }
                     ?>
-                    <li class="nav-item dropdown">
-                        <a
-                                class="nav-link dropdown-toggle mt-1"
-                                href="#"
-                                id="navbarDropdownMenuLink"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                        >
-                            <?php
-                            if (isset($_GET['language'])) {
-                                if ($_GET['language'] == 'eng') {
-                                    echo '
-                                    <img
-                                            src="https://flagcdn.com/24x18/gb.png"
-                                            srcset="https://flagcdn.com/48x36/gb.png 2x,
-                                            https://flagcdn.com/72x54/gb.png 3x"
-                                            width="24"
-                                            height="18"
-                                            alt="United Kingdom">
-                                    ';
-                                }
-
-                                if ($_GET['language'] == 'ukr') {
-                                    echo '
-                                    <img
-                                            src="https://flagcdn.com/24x18/ua.png"
-                                            srcset="https://flagcdn.com/48x36/ua.png 2x,
-                                            https://flagcdn.com/72x54/ua.png 3x"
-                                            width="24"
-                                            height="18"
-                                            alt="Ukraine">
-                                    ';
-                                }
-                            }
-                            ?>
-
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li>
-                                <a class="dropdown-item" href="?language=eng">
-                                    <img
-                                            src="https://flagcdn.com/24x18/gb.png"
-                                            srcset="https://flagcdn.com/48x36/gb.png 2x,
-                                            https://flagcdn.com/72x54/gb.png 3x"
-                                            width="24"
-                                            height="18"
-                                            alt="United Kingdom">
-                                    English
-                                    <?php
-                                    if (!isset($_GET['language']))
-                                        echo '<i class="bi bi-check text-success fs-4"></i>';
-                                    else {
-                                        if ($_GET['language'] == 'eng')
-                                            echo '<i class="bi bi-check text-success fs-4"></i>';
-                                    }
-                                    ?>
-                                </a>
-                            </li>
-                            <li>
-                                <hr class="dropdown-divider"/>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="?language=ukr">
-                                    <img
-                                            src="https://flagcdn.com/24x18/ua.png"
-                                            srcset="https://flagcdn.com/48x36/ua.png 2x,
-                                            https://flagcdn.com/72x54/ua.png 3x"
-                                            width="24"
-                                            height="18"
-                                            alt="Ukraine">
-                                    Українська
-                                    <?php
-                                    if (isset($_GET['language']) && $_GET['language'] == 'ukr')
-                                        echo '<i class="bi bi-check text-success fs-4"></i>';
-                                    ?>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/site/settings">
-                            <span><i class="bi bi-gear fs-4"></i></span>
-                        </a>
+                        <?php
+                        if (empty($language)) {
+                            echo "
+                            <a class='nav-link' href='/site/eng/settings' data-toggle='tooltip' data-placement='top' title='Settings'>
+                                <span><i class='bi bi-gear fs-4'></i></span>
+                            </a>
+                            ";
+                        } else {
+                            echo "
+                            <a class='nav-link' href='/site/$language/settings' data-toggle='tooltip' data-placement='top' title='Register'>
+                                <span><i class='bi bi-gear fs-4'></i></span>
+                            </a>
+                            ";
+                        }
+                        ?>
                     </li>
                 </ul>
             </div>
@@ -252,7 +220,6 @@
     </div>
     <!-- Copyright -->
 </footer>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous">
