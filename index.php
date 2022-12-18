bc35787b717d9258d489ef3cdc1a73a052c874bd
Andrii Babushko <andriibabushko@gmail.com>
2022-12-18 17:18:04 +0200

    Some little changes to main index.php file

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
+
+use core\DB;
+
+include 'config/database.php';
+
 spl_autoload_register(function ($className) {
     $path = $className . ".php";
     include_once($path);
 });
+
+//$res = $db->insert('User', [
+//    'first_name' => 'Andrii',
+//    'last_name' => 'Babushko',
+//    'email' => 'andriibabushko@gmail.com',
+//    'phone_number' => '+380638179858',
+//    'bio' => 'My name is Andrii. I am Junior Web Developer.',
+//    'password' => 'Andrey1229576',
+//    'is_admin' => 0]);
+//var_dump($res);
 
 $core = core\Core::getInstance();
 $core->initialize();
