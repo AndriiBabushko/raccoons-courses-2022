e6f3ea693e0e1210ccbc2c9ee311c2a35becc8dc
Andrii Babushko <andriibabushko@gmail.com>
2022-12-30 14:41:10 +0200

    Modify main index.php

--- index.php
+++ index.php
@@ -6,18 +6,9 @@ include 'config/database.php';
 
 spl_autoload_register(function ($className) {
     $path = $className . ".php";
-    include_once($path);
+    if(file_exists($path))
+        include_once($path);
 });
-
-//$res = $db->insert('User', [
-//    'first_name' => 'Andrii',
-//    'last_name' => 'Babushko',
-//    'email' => 'andriibabushko@gmail.com',
-//    'phone_number' => '+380638179858',
-//    'bio' => 'My name is Andrii. I am Junior Web Developer.',
-//    'password' => 'Andrey1229576',
-//    'is_admin' => 0]);
-//var_dump($res);
 
 $core = core\Core::getInstance();
 $core->initialize();
