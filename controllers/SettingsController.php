a587243bff2bff8b7d304728088dc99665132605
Andrii Babushko <andriibabushko@gmail.com>
2022-12-30 14:44:38 +0200

    Modify settings page controller

---
+++ controllers/SettingsController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace controllers;
+
+use core\Controller;
+use core\Core;
+use models\User;
+
+class SettingsController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function indexAction(): bool|string
+    {
+        if (Core::getInstance()->requestMethod === 'POST') {
+            $id_user = User::getCurrentAuthUser()['id_user'];
+            $model = $_POST;
+
+            $updateStatus = User::updateUser($id_user, $model, $_FILES['avatar']['tmp_name'], $_FILES['avatar']['name']);
+
+            if ($updateStatus) {
+                $user = User::getUserById($id_user);
+                User::authUser($user);
+
+                return $this->renderView('updateUserStatus', [
+                    'updateStatus' => true
+                ]);
+            }
+
+            return $this->renderView('updateUserStatus', [
+                'updateStatus' => false
+            ]);
+        }
+
+        return $this->render();
+    }
+
+    public function deleteUserAction(): bool|string
+    {
+        if (User::isUserAuth()) {
+            $deleteStatus = User::deleteUser(User::getCurrentAuthUser()['id_user']);
+
+            if ($deleteStatus) {
+                User::logoutUser();
+
+                return $this->renderView("deleteUserStatus", [
+                    'deleteStatus' => true
+                ]);
+            }
+
+            return $this->renderView("deleteUserStatus", [
+                'deleteStatus' => false
+            ]);
+        }
+
+        return $this->renderView("index");
+    }
+}
