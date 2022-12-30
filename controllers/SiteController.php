728ad341d6a4674d636ed44d010c6406095306f8
Andrii Babushko <andriibabushko@gmail.com>
2022-12-30 14:38:22 +0200

    Modify SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,6 +3,9 @@
 namespace controllers;
 
 use core\Controller;
+use core\Core;
+use JetBrains\PhpStorm\NoReturn;
+use models\User;
 
 class SiteController extends Controller
 {
@@ -16,20 +19,15 @@ class SiteController extends Controller
         return $this->render();
     }
 
-    public function settingsAction(): bool|string
-    {
-        return $this->render(null, ['language' => $this->language, 'theme' => $this->theme]);
-    }
-
     public function errorAction($code): bool|string
     {
-        switch ($code){
-            case 404: {
+        switch ($code) {
+            case 403:
+                return $this->render(null, ['error_message' => 'Error 403. Access denied!']);
+            case 404:
                 return $this->render(null, ['error_message' => 'Error 404. Page not found!']);
-            }
-            default:{
-                return $this->render(null, ['error_message' => 'Something went wrong']);
-            }
+            default:
+                return $this->render(null, ['error_message' => 'Something went wrong!']);
         }
     }
 }
