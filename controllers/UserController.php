0fbe615012cf1cd81e955ffb4b26bc1849291c23
Andrii Babushko <andriibabushko@gmail.com>
2022-12-30 14:38:12 +0200

    Modify UserController.php

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,6 +3,10 @@
 namespace controllers;
 
 use core\Controller;
+use core\Core;
+use core\Utils;
+use JetBrains\PhpStorm\NoReturn;
+use models\User;
 
 class UserController extends Controller
 {
@@ -11,23 +15,76 @@ class UserController extends Controller
         parent::__construct();
     }
 
-    public function indexAction(): bool|string
-    {
-        return $this->render();
-    }
-
-    public function formAction(): bool|string
-    {
-        return $this->render();
-    }
-
     public function loginAction(): bool|string
     {
+        if (User::isUserAuth()) {
+            $this->redirect('/');
+        }
+
+        if (Core::getInstance()->requestMethod === 'POST') {
+            $user = User::getUserByEmailAndPassword($_POST['email'], $_POST['password']);
+
+            if (empty($user)) {
+                $error = null;
+
+                if (Utils::ifLanguageEqual('ukr'))
+                    $error = 'Логін та пароль неправильні або не існують!';
+
+                if (Utils::ifLanguageEqual('eng'))
+                    $error = 'Login and password are incorrect or do not exist!';
+
+                $model = $_POST;
+                return $this->render(null, [
+                    'error' => $error,
+                    'model' => $model
+                ]);
+            } else {
+                User::authUser($user);
+
+                $this->redirect('/');
+            }
+        }
+
         return $this->render();
     }
 
     public function registerAction(): bool|string
     {
+        if (User::isUserAuth()) {
+            $this->redirect('/');
+        }
+
+        if (Core::getInstance()->requestMethod === 'POST') {
+            $errors = [];
+            // echo "<pre>" . var_dump($_POST) . "</pre>";
+            if (User::isEmailExists($_POST['email'])) {
+                if (Utils::ifLanguageEqual('ukr'))
+                    $errors['email'] = 'Дана електронна пошта вже зайнята.';
+
+                if (Utils::ifLanguageEqual('eng'))
+                    $errors['email'] = 'This email address is already used.';
+            }
+
+            if (count($errors) > 0) {
+                $model = $_POST;
+
+                return $this->render(null, [
+                    'errors' => $errors,
+                    'model' => $model
+                ]);
+            } else {
+                User::addUser($_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['phone_number'], $_POST['password']);
+
+                return $this->renderView("registerStatus");
+            }
+        }
+
         return $this->render();
+    }
+
+    #[NoReturn] public function logoutAction()
+    {
+        User::logoutUser();
+        $this->redirect('/');
     }
 }
