448160b231d61530392ff7a353e3f9c165deb363
Andrii Babushko <andriibabushko@gmail.com>
2023-01-01 20:15:06 +0200

    Modify good model

--- models/Good.php
+++ models/Good.php
@@ -8,19 +8,43 @@ class Good
 {
     protected static string $tableName = "Good";
 
-    public static function addGood(array $addData): bool
+    public static function addGood(array $addData, string $imgPath, string $imgName): bool
     {
+        if (!file_exists("static/img/category/$imgName")) {
+            $newImgPath = "static/img/category/$imgName";
+            $addData += ['photo' => $imgName];
+            move_uploaded_file($imgPath, $newImgPath);
+        }
+
         return Core::getInstance()->db->insert(self::$tableName, $addData);
     }
 
-    public static function updateGood(int $id_good, array $updateData): bool
+    public static function updateGood(int $id_good, array $updateData, string $imgPath, string $imgName): bool
     {
+        $goodPhotoName = self::getGoodById($id_good)['photo'];
+        $photoPath = "static/img/courses/$goodPhotoName";
+        if (file_exists($photoPath) && $goodPhotoName !== "no_image.png")
+            unlink($photoPath);
+
+        if (!file_exists("static/img/category/$imgName")) {
+            $newImgPath = "static/img/courses/$imgName";
+            $updateData += ['photo' => $imgName];
+            move_uploaded_file($imgPath, $newImgPath);
+        }
+
         return Core::getInstance()->db->update(self::$tableName, $updateData, [
             'id_good' => $id_good
         ]);
     }
+
     public static function deleteGood(int $id_good): bool
     {
+        $imgName = self::getGoodById($id_good)['photo'];
+        $imgPath = "static/img/category/$imgName";
+
+        if(file_exists($imgPath))
+            unlink($imgPath);
+
         return Core::getInstance()->db->delete(self::$tableName, [
             'id_good' => $id_good
         ]);
@@ -41,5 +65,14 @@ class Good
     public static function getGoods(): bool|array
     {
         return Core::getInstance()->db->select(self::$tableName);
+    }
+
+    public static function verifyGoodByName($name): bool
+    {
+        $good = Core::getInstance()->db->select(self::$tableName, '*', [
+            'name' => $name
+        ]);
+
+        return empty($good);
     }
 }
