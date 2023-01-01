9b48def4814469cc9c333edc51e21cb434691f4b
Andrii Babushko <andriibabushko@gmail.com>
2023-01-01 20:20:09 +0200

    Modify Courses controller

--- controllers/CoursesController.php
+++ controllers/CoursesController.php
@@ -3,6 +3,9 @@
 namespace controllers;
 
 use core\Controller;
+use core\Core;
+use core\Utils;
+use models\Error;
 use models\Good;
 use models\User;
 
@@ -16,6 +19,7 @@ class CoursesController extends Controller
     public function indexAction(): bool|string
     {
         $goods = Good::getGoods();
+
         return $this->render(null, [
             'goods' => $goods
         ]);
@@ -26,25 +30,53 @@ class CoursesController extends Controller
         return $this->render();
     }
 
-    public function addAction(): \models\Error|bool|string
+    public function addAction(): Error|bool|string
     {
-        if(!User::isAdmin())
+        if (!User::isAdmin())
+            return $this->error(403);
+
+        if (Core::getInstance()->requestMethod === 'POST') {
+            $model = $_POST;
+
+            $errors = [];
+
+            if (Good::verifyGoodByName($model['name'])) {
+                $addGoodStatus = Good::addGood($model, $_FILES['photo']['tmp_name'], $_FILES['photo']['name']);
+
+                if ($addGoodStatus)
+                    $this->redirect("/courses/$this->language/index");
+
+                $errors += Utils::generateError('somethingWrong', [
+                    'ukr' => 'Щось пішло не так! Спробуйте ще раз!',
+                    'eng' => 'Something went wrong! Try again!'
+                ]);
+            }
+
+            $errors += Utils::generateError('name', [
+                'ukr' => 'Введена назва категорії вже існує!',
+                'eng' => 'The entered category name already exists!'
+            ]);
+
+            return $this->render(null, [
+                'model' => $model,
+                'errors' => $errors
+            ]);
+        }
+
+        return $this->render();
+    }
+
+    public function updateAction(): Error|bool|string
+    {
+        if (!User::isAdmin())
             return $this->error(403);
 
         return $this->render();
     }
 
-    public function updateAction(): \models\Error|bool|string
+    public function deleteAction(): Error|bool|string
     {
-        if(!User::isAdmin())
-            return $this->error(403);
-
-        return $this->render();
-    }
-
-    public function deleteAction(): \models\Error|bool|string
-    {
-        if(!User::isAdmin())
+        if (!User::isAdmin())
             return $this->error(403);
 
         return $this->render();
