c70f841395dd1084f21d92da7ab8c108c2edce25
Andrii Babushko <andriibabushko@gmail.com>
2023-01-01 20:14:34 +0200

    Modify category and courses index files

--- views/light/courses/eng/index.php
+++ views/light/courses/eng/index.php
@@ -1,43 +1,34 @@
 <?php
 /**
- * @var array $goods ;
- * @var array $user ;
+ * @var array $goods
  */
+
+use models\User;
+
+$user = null;
+if (User::isUserAuth())
+    $user = User::getCurrentAuthUser();
 ?>
 
 <div class="container">
     <div class="row mt-3">
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-            <div class="p-2">
-                <h1 class="title fs-4 text-center mb-2 pb-2 border-bottom" id="productsTitle">All products:</h1>
+        <div class="p-2">
+            <h1 class="title fs-4 text-center mb-2 pb-2 border-bottom border-2" id="productsTitle">All products:</h1>
 
-                <div class="mb-3 pb-3 text-start fs-5 fw-normal border-bottom" role="search" id="searchBlock">
-                    <label for="search" class="form-label">Search for here:</label>
-                    <input type="search" name="search" class="form-control me-2" id="search" placeholder="Search" aria-label="Search">
-                </div>
 
+
+
+
+
+
+            <?php if (!empty($user) && $user['is_admin'] === 1): ?>
                 <div class="mb-3 pb-3 text-start fs-5 fw-normal border-bottom">
-                    <label class="form-label" for="sortBy">Sort by:</label>
-                    <select class="form-select form-select-lg" name="sortBy" id="sortBy">
-                        <option selected>Choose one</option>
-                        <option value="title">By Title</option>
-                        <option value="price">By Price</option>
-                        <option value="time">By Time</option>
-                    </select>
+                    <a class="btn btn-success w-100" href="/category/eng/add">
+                        <i class="bi bi-plus-circle"></i>
+                        Add new category
+                    </a>
                 </div>
-
-                <div class="mb-3 pb-3 text-start fs-5 fw-normal border-bottom">
-                    <label class="form-label">Sorting type:</label>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="sortingType" id="ascendingSort" value="ascendingSort" checked>
-                        <label class="form-check-label" for="ascendingSort">Ascending sort</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="sortingType" id="descendingSort" value="descendingSort">
-                        <label class="form-check-label" for="descendingSort">Descending sort</label>
-                    </div>
-                </div>
-            </div>
+            <?php endif; ?>
         </div>
     </div>
 
