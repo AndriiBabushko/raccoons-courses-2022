33ff24431574453dbc3336ca6136b2256eec9f97
Andrii Babushko <andriibabushko@gmail.com>
2023-01-01 20:14:57 +0200

    Modify utils class

--- core/Utils.php
+++ core/Utils.php
@@ -4,19 +4,30 @@ namespace core;
 
 class Utils
 {
-    public static function filterArray($array, $fieldList): array
+    public static function filterArray(array $array, array $fieldList): array
     {
         $newArray = [];
-        foreach ($array as $key => $value){
-            if(in_array($key, $fieldList))
+        foreach ($array as $key => $value) {
+            if (in_array($key, $fieldList))
                 $newArray [$key] = $value;
         }
 
         return $newArray;
     }
 
-    public static function ifLanguageEqual($language): bool
+    public static function ifLanguageEqual(string $language): bool
     {
         return \core\Core::getInstance()->app['language'] === $language;
+    }
+
+    public static function generateError(string $errorName, array $errorsTextList): array
+    {
+        $errors = [];
+
+        foreach ($errorsTextList as $key => $value)
+            if (Utils::ifLanguageEqual($key))
+                $errors[$errorName] = $value;
+
+        return $errors;
     }
 }
