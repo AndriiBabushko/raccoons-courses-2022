f45f29637314d652a515856f48952a2b42783afd
Andrii Babushko <andriibabushko@gmail.com>
2023-01-03 09:20:09 +0200

    Complete delete good page

---
+++ views/light/courses/eng/delete.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 w-100 position-relative">
+        <div class="form-container d-flex justify-content-center" id="goodDeleteContainer">
+            <form action="" method="post" enctype="multipart/form-data" class="d-flex flex-column align-items-center justify-content-center position-relative"
+                  id="goodDeleteForm">
+                <div id="categoryFormImg"></div>
+
+                <div class="" id="content">
+                    <h3 class="h3 my-4 pb-1 text-center title">Delete good form!</h3>
+
+                    <div class="form-group mb-3">
+                        <label for="buttonDeleteGood" id="labelDeleteGood" class="form-label fs-5">Are you sure to delete good?</label>
+                        <div class="d-flex flex-wrap justify-content-evenly w-100 mt-3">
+                            <button type='submit' id='buttonDeleteGood' class='btn btn-danger'>Delete good</button>
+                            <a href="/courses/eng/index" id='buttonCancelDeleteGood' class='btn btn-secondary'>Get back to courses page</a>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
