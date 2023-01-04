75ca577acb89d2045a9891706535fcf804b6b330
Andrii Babushko <andriibabushko@gmail.com>
2023-01-04 10:58:15 +0200

    Modify Cart Controller logic to add goods to cart

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -3,6 +3,10 @@
 namespace controllers;
 
 use core\Controller;
+use core\Utils;
+use models\Cart;
+use models\Good;
+use models\User;
 
 class CartController extends Controller
 {
@@ -13,6 +17,59 @@ class CartController extends Controller
 
     public function indexAction(): bool|string
     {
-        return $this->render();
+        if (!User::isUserAuth())
+            $this->redirect('/');
+
+        //echo "<pre>";
+        $errors = [];
+        $id_user = intval(User::getCurrentAuthUser()['id_user']);
+
+        if (isset($_GET['id_good'])) {
+            $id_good = intval($_GET['id_good']);
+            $good = Good::getGoodById($id_good);
+
+            if (!Cart::isGoodInCart($id_user, $id_good)) {
+                $cartGoods = Cart::getCartGoodsByUserID($id_user);
+                if ($cartGoods !== null)
+                    $goods = unserialize($cartGoods);
+
+                if (!empty($goods))
+                    $goods[] = $good;
+                else
+                    $goods = [$good];
+
+                $model = ['goods' => serialize($goods)];
+                $cartUpdateStatus = Cart::updateCartByUserID($id_user, $model);
+
+                if ($cartUpdateStatus) {
+                    return $this->render(null, [
+                        'goods' => $goods,
+                        'errors' => $errors
+                    ]);
+                } else {
+                    $errors += Utils::generateError('somethingWrong', [
+                        'ukr' => 'Щось пішло не так! Спробуйте ще раз!',
+                        'eng' => 'Something went wrong! Try again!'
+                    ]);
+                }
+            } else {
+                $errors += Utils::generateError('goodExists', [
+                    'ukr' => 'Даний товар вже існує в корзині! Поверніться та виберіть інший.',
+                    'eng' => 'This product already exists in the basket! Go back and choose another one.'
+                ]);
+            }
+        }
+
+        return $this->render(null, [
+            'errors' => $errors,
+            'goods' => unserialize(Cart::getCartGoodsByUserID($id_user))
+        ]);
+    }
+
+    public function deleteAction()
+    {
+
+
+        return $this->renderView("index");
     }
 }
