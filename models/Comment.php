e6d5a8c16ef89908908c5e5a618ced59eda9d67e
Andrii Babushko <andriibabushko@gmail.com>
2023-01-08 10:32:12 +0200

    Add Comment model

---
+++ models/Comment.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace models;
+
+use core\Core;
+
+class Comment
+{
+    protected static string $tableName = "Comment";
+
+    public static function addComment(array $tableFields): bool
+    {
+        return Core::getInstance()->db->insert(self::$tableName, $tableFields);
+    }
+
+    public static function updateCommentByGoodId(int $id_good, array $tableFields): bool
+    {
+        return Core::getInstance()->db->update(self::$tableName, $tableFields, [
+            'id_good' => $id_good
+        ]);
+    }
+
+    public static function updateCommentByCommentId(int $id_comment, array $tableFields): bool
+    {
+        return Core::getInstance()->db->update(self::$tableName, $tableFields, [
+            'id_comment' => $id_comment
+        ]);
+    }
+
+    public static function deleteCommentByGoodId(int $id_good): bool
+    {
+        return Core::getInstance()->db->delete(self::$tableName, [
+            'id_good' => $id_good
+        ]);
+    }
+
+    public static function deleteCommentByCommentId(int $id_comment): bool
+    {
+        return Core::getInstance()->db->delete(self::$tableName, [
+            'id_comment' => $id_comment
+        ]);
+    }
+
+    public static function getComments(): bool|array
+    {
+        return Core::getInstance()->db->select(self::$tableName);
+    }
+
+    public static function getCommentsByGoodId(int $id_good): bool|array
+    {
+        return Core::getInstance()->db->select(self::$tableName, '*', [
+            'id_good' => $id_good
+        ]);
+    }
+}
