05df7359b7f744d81bd96431346a912e4682cd3e
Andrii Babushko <andriibabushko@gmail.com>
2023-01-08 10:25:57 +0200

    Fix user update in SettingsController.php

--- controllers/SettingsController.php
+++ controllers/SettingsController.php
@@ -20,17 +20,21 @@ class SettingsController extends Controller
             $id_user = User::getCurrentAuthUser()['id_user'];
             $model = $_POST;
 
-            if(Utils::checkImgExtension($_FILES['avatar']['name'])) {
-                $updateStatus = User::updateUser($id_user, $model, $_FILES['avatar']['tmp_name'], $_FILES['avatar']['name']);
-
-                if ($updateStatus) {
-                    $user = User::getUserById($id_user);
-                    User::authUser($user);
-
-                    return $this->renderView('updateUserStatus', [
-                        'updateStatus' => true
-                    ]);
+            if (empty($_FILES['photo']['name']) && empty($_FILES['photo']['tmp_name'])) {
+                $updateStatus = User::updateUser($id_user, $model);
+            } else {
+                if (Utils::checkImgExtension($_FILES['avatar']['name'])) {
+                    $updateStatus = User::updateUser($id_user, $model, $_FILES['avatar']['tmp_name'], $_FILES['avatar']['name']);
                 }
+            }
+
+            if (!empty($updateStatus) && $updateStatus) {
+                $user = User::getUserById($id_user);
+                User::authUser($user);
+
+                return $this->renderView('updateUserStatus', [
+                    'updateStatus' => true
+                ]);
             }
 
             return $this->renderView('updateUserStatus', [
@@ -44,7 +48,8 @@ class SettingsController extends Controller
         ]);
     }
 
-    public function deleteUserAction(): bool|string
+    public
+    function deleteUserAction(): bool|string
     {
         if (User::isUserAuth()) {
             $deleteStatus = User::deleteUser(User::getCurrentAuthUser()['id_user']);
