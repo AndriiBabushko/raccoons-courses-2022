e2c3367836d0d64695e97085aa54ab29d5083edb
Andrii Babushko <andriibabushko@gmail.com>
2023-01-09 16:50:36 +0200

    Final view course page

--- views/light/courses/eng/view.php
+++ views/light/courses/eng/view.php
@@ -2,52 +2,51 @@
 /**
  * @var int $idGood
  * @var string $goodImgName
+ * @var array $model
  * @var array $videos
  * @var array $currentVideo
  * @var array $comments
  * @var array $courseMaker
+ * @var array $currentUser
  * @var array $errors
  * @var bool $courseAccess
- * @var bool $isAdmin
+ * @var bool $isCourseMaker
  */
+// echo "<pre>";
+//var_dump($errors);
 ?>
 
 <div class="container-fluid">
     <div class="row my-2">
         <?php if (empty($videos)): ?>
-            <div class="row">
-                <div class="col">
-                    <div class="alert alert-warning my-2 w-100 text-center">
-                        <i class="bi bi-exclamation-diamond-fill"></i>
-                        There are no videos on this course yet! Check back later :)
-                    </div>
+            <div class="col-12">
+                <div class="alert alert-warning my-2 w-100 text-center">
+                    <i class="bi bi-exclamation-diamond-fill"></i>
+                    There are no videos on this course yet! Check back later :)
                 </div>
             </div>
+
         <?php endif; ?>
 
         <?php if (!empty($errors['notExist'])): ?>
-            <div class="row">
-                <div class="col">
-                    <div class="alert alert-danger my-2 w-100 text-center">
-                        <i class="bi bi-exclamation-diamond-fill"></i>
-                        <?php echo $errors['notExist']; ?>
-                    </div>
+            <div class="col-12">
+                <div class="alert alert-danger my-2 w-100 text-center">
+                    <i class="bi bi-exclamation-diamond-fill"></i>
+                    <?php echo $errors['notExist']; ?>
                 </div>
             </div>
         <?php endif; ?>
 
-        <?php if (!$courseAccess): ?>
-            <div class="row">
-                <div class="col">
-                    <div class="alert alert-warning my-2 w-100 text-center">
-                        <i class="bi bi-exclamation-diamond-fill"></i>
-                        Buy the course for full access!
-                    </div>
+        <?php if (!$courseAccess && !$isCourseMaker): ?>
+            <div class="col-12">
+                <div class="alert alert-warning my-2 w-100 text-center">
+                    <i class="bi bi-exclamation-diamond-fill"></i>
+                    Buy the course for full access!
                 </div>
             </div>
         <?php endif; ?>
 
-        <?php if (empty($errors) && !empty($videos)): ?>
+        <?php if (!empty($videos) && empty($errors['notExist'])): ?>
             <div class="col-12 col-xxl-9 col-xl-9 col-lg-9 col-md-12 col-sm-12">
                 <div class="video-container paused" data-volume-level="up">
                     <img src="/static/img/courses/<?php echo $goodImgName; ?>" alt="<?php echo $goodImgName; ?>" class="thumbnail-img">
@@ -93,7 +92,7 @@
             <div class="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 ps-xxl-0 ps-xl-0 ps-lg-0">
                 <div class="view-video-menu">
                     <ul class="nav nav-pills d-flex flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start w-100">
-                        <?php if ($courseAccess): ?>
+                        <?php if ($courseAccess || $isCourseMaker): ?>
                             <?php foreach ($videos as $video): ?>
                                 <li class="nav-item w-100 settings-item">
                                     <a href="/courses/eng/view/<?php echo $idGood; ?>?id_video=<?php echo $video['id_video'] ?>"
@@ -105,7 +104,7 @@
                             <?php endforeach; ?>
                         <?php endif; ?>
 
-                        <?php if (!$courseAccess): ?>
+                        <?php if (!$courseAccess && !$isCourseMaker): ?>
                             <?php foreach ($videos as $video): ?>
                                 <li class="nav-item w-100 settings-item">
                                     <a href="/courses/eng/view/<?php echo $idGood; ?>?id_video=<?php echo $video['id_video'] ?>"
@@ -123,7 +122,7 @@
         <?php endif; ?>
     </div>
 
-    <?php if (!empty($isAdmin) && $isAdmin): ?>
+    <?php if (!empty($isCourseMaker) && $isCourseMaker): ?>
         <div class="row mt-2">
             <div class="col-12 col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 mb-2">
                 <a class="btn btn-success w-100" href="/video/eng/add?id_good=<?php echo $idGood; ?>">
@@ -150,47 +149,103 @@
 
     <?php if (!empty($courseMaker)): ?>
         <div class="col-12">
-            <div class="course-maker">
-                <div class="d-flex flex-column align-items-center justify-content-center w-100" id="userAvatar">
-                    <div class="d-flex flex-column align-items-center justify-content-between w-100" id="userAvatarBlock">
-                        <div class="title w-100 mb-2">
-                            <h3>
-                                Course creator
-                            </h3>
-                        </div>
+            <div class="row mb-2">
+                <div class="col-12 col-xxl-6 col-xl-6 col-lg-6 col-md-6 course-maker">
+                    <div class="course-maker-title w-100 my-3">
+                        <h3>Course Creator</h3>
+                    </div>
 
-                        <div class="card border-2">
-                            <div class="card-body">
-                                <img
-                                    <?php $userName = ucfirst($courseMaker['first_name']) . ' ' . ucfirst($courseMaker['last_name']); ?>
-                                        src='/static/img/user/<?php echo $courseMaker['avatar']; ?>'
-                                        alt='<?php echo $userName; ?>'
-                                        class='rounded-circle'
-                                        id='courseMakerImg'
-                                        title='<?php echo $userName; ?>'
-                                        style="cursor: default;"
-                                >
-                                <h5 class="card-title">
-                                    <span class='d-none d-sm-inline mb-2 h3 text-dark text-center'><?php echo $userName; ?></span>
-                                </h5>
-                                <hr>
-                                <p class="card-text"><?php echo $courseMaker['bio']; ?></p>
-                                <?php if (!empty($user) && $user['is_admin'] === 1): ?>
+                    <div class="d-flex flex-column align-items-center justify-content-center" id="userAvatar">
+                        <div class="d-flex flex-column align-items-center justify-content-between w-100 m-0" id="userAvatarBlock">
+                            <div class="course-maker-block border-2 w-100">
+                                <div class="card-body">
+                                    <img
+                                        <?php $userName = ucfirst($courseMaker['first_name']) . ' ' . ucfirst($courseMaker['last_name']); ?>
+                                            src='/static/img/<?php if ($courseMaker['avatar'] === 'default_user_img.png') echo $courseMaker['avatar']; else echo 'user/' . $courseMaker['avatar']; ?>'
+                                            alt='<?php echo $userName; ?>'
+                                            class='rounded-circle'
+                                            id='courseMakerImg'
+                                            title='<?php echo $userName; ?>'
+                                            style="cursor: default;"
+                                    >
+                                    <h5 class="card-title">
+                                        <span class='d-none d-sm-inline mb-2 h3 text-dark text-center'><?php echo $userName; ?></span>
+                                    </h5>
                                     <hr>
-                                    <div class="d-flex justify-content-between admin-buttons">
-                                        <a class="btn btn-primary" href="/category/eng/update?id_category=<?php echo $category['id_category'] ?>"><i class="bi bi-pencil"></i></a>
-                                        <a class="btn btn-danger" href="/category/eng/delete?id_category=<?php echo $category['id_category'] ?>"><i class="bi bi-trash"></i></a>
-                                    </div>
-                                <?php endif; ?>
+                                    <p class="card-text"><?php echo $courseMaker['bio']; ?></p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
 
+                <div class="col-12 col-xxl-6 col-xl-6 col-lg-6 col-md-6 comments">
+                    <div class="comments-title w-100 my-3">
+                        <h3>Course Comments</h3>
+                    </div>
+
+                    <div class="comment-block border-2">
+                        <div class="comment-form-container">
+                            <form action="" method="post" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <label for="comment" id="labelComment" class="form-label label">Leave comment</label>
+                                    <textarea class="input form-control short" name="comment" id="comment"
+                                              placeholder="Leave a comment here :)"><?php if (!empty($model)) echo $model['comment']; ?></textarea>
+                                </div>
+
+                                <div class="form-group mt-2">
+                                    <button type='submit' id='buttonSendComment' class='btn btn-secondary w-100'>Send comment</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <?php if (empty($comments)): ?>
+                            <div class="alert alert-dark m-2 text-center w-100">
+                                <i class="bi bi-exclamation-diamond-fill"></i>
+                                There are no comments on the course yet! Come back later or write your own :>
+                            </div>
+                        <?php endif; ?>
+
+
+                        <?php if (!empty($errors['commentError'])): ?>
+                            <div class="alert alert-danger m-2 text-center w-100">
+                                <i class="bi bi-exclamation-diamond-fill"></i>
+                                <?php echo $errors['commentError']; ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <?php
+                        if (!empty($comments))
+                            foreach ($comments as $comment):?>
+                                <div class="comment mt-3 w-100">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between">
+                                            <div>
+                                                <h5 class="card-title"><?php echo ucfirst($comment['first_name']) . ' ' . ucfirst($comment['last_name']); ?></h5>
+                                                <p class="card-subtitle"><?php echo date('d.m.Y H:m:s', strtotime($comment['date'])); ?></p>
+                                            </div>
+                                            <?php if ($isCourseMaker || $currentUser['id_user'] == $comment['id_user']): ?>
+                                                <div class="d-flex align-items-center">
+                                                    <a class="btn btn-danger"
+                                                       href="/courses/eng/deleteComment?id_comment=<?php echo $comment['id_comment']; ?>&id_good=<?php echo $idGood; ?>"
+                                                    >
+                                                        <i class="bi bi-trash"></i>
+                                                    </a>
+                                                </div>
+                                            <?php endif; ?>
+                                        </div>
+                                        <hr>
+                                        <p class="card-text"><?php echo $comment['comment'] ?></p>
+                                    </div>
+                                </div>
+                            <?php endforeach;
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
     <?php endif; ?>
 </div>
 
-<script defer><?php require_once "static/js/videoPlayer.js"; ?></script>
-<script defer><?php require_once "static/js/videoPage.js"; ?></script>
+<script><?php require_once "static/js/videoPlayer.js"; ?></script>
+<script><?php require_once "static/js/videoPage.js"; ?></script>
